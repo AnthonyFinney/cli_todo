d6352118c54bbe46f24a7871c522771e3e8580a7
AnthonyFinney <anthonyrockfinnie0@gmail.com>
2025-10-17 21:11:20 +0600

    add the test file and the todo's

---
+++ test/store.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from "bun:test";
+
+describe("store", () => {
+    test.todo("creates the store", () => {});
+
+    test.todo("read item from the store", () => {});
+
+    test.todo("remove item from the store", () => {});
+});
---
+++ test/write.test.ts
@@ -0,0 +1,5 @@
+import { describe, expect, test } from "bun:test";
+
+describe("write", () => {
+    test.todo("write on the json file", () => {});
+});
